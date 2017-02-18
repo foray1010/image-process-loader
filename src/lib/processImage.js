e832b844190abe9792a1acffeddcdae73d848237
foray1010 <foray1010@users.noreply.github.com>
2017-02-18 10:41:27 +0800

    Show better debug message when wrong options are used

--- src/lib/processImage.js
+++ src/lib/processImage.js
@@ -5,6 +5,10 @@ const sharp = require('sharp')
 module.exports = (content, query) => {
   return Object.keys(query)
     .reduce((sharpInstance, methodName) => {
+      if (!sharpInstance[methodName]) {
+        throw new Error(`Method "${methodName}" is undefined`)
+      }
+
       const params = (
         Array.isArray(query[methodName]) ?
           query[methodName] :
